fa1803aa64789360a54559579b691bd32d04893c
jcleme <clemensjosh@protonmail.com>
2024-05-12 22:28:24 -0400

    (extremely) basic iced UI
    play button, does actually play audio, but not audiably. not sure why,
    suspect it has something to do with threads, as audio does sucessfully
    play in main(), just not when called from iced.

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,19 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use lofty::read_from_path;
-use lofty::{error::LoftyError, file::TaggedFileExt};
-
 use figment::{
     providers::{Format, Toml},
     Figment,
 };
+use lofty::read_from_path;
+use lofty::{error::LoftyError, file::TaggedFileExt};
 use rodio::Sink;
 use serde::{Deserialize, Serialize};
 use std::{io::BufReader, path::Path};
 use thiserror::Error;
+
+use iced::widget::{button, container, row};
+use iced::{Element, Length, Sandbox, Settings};
 
 #[derive(Debug, Serialize, Deserialize)]
 struct GlobalSettings {
@@ -19,15 +21,24 @@ struct GlobalSettings {
                             // theme: VisualTheme
 }
 
+impl Default for GlobalSettings {
+    fn default() -> Self {
+        Self {
+            folder_to_scan: String::from("./"),
+        }
+    }
+}
+
 struct PlaybackSettings {
     volume: f32, // lets leave this at 1.0 for now
     speed: f32,  // lets leave this at 1.0 for now
 }
 
-impl Default for GlobalSettings {
+impl Default for PlaybackSettings {
     fn default() -> Self {
         Self {
-            folder_to_scan: String::from("./"),
+            volume: 1.0,
+            speed: 1.0,
         }
     }
 }
@@ -56,52 +67,93 @@ impl serde::Serialize for JbError {
     }
 }
 
-struct Jukebox(Sink);
-// struct PlayQueue
-
-#[derive(serde::Serialize)]
+#[derive(serde::Serialize, Debug, Clone)]
 pub enum PlaybackState {
     Play,
     Pause,
     Stop,
 }
 
-// Tauri Commands
+#[derive(Debug, Clone)]
+pub enum Message {
+    PlaybackState(PlaybackState),
+    AddSongToQueue(String),
+}
 
-#[tauri::command]
+struct Jukebox {
+    sink: Sink,
+    global_settings: GlobalSettings,
+    playback_settings: PlaybackSettings,
+}
+
+impl Sandbox for Jukebox {
+    type Message = Message;
+
+    fn new() -> Self {
+        // Rodio sink, plays the audio
+        let (_stream, handle) = rodio::OutputStream::try_default().unwrap();
+        let sink = Sink::try_new(&handle).unwrap();
+
+        println!("new() called, sink created");
+
+        sink.append(
+            rodio::Decoder::new(BufReader::new(std::fs::File::open("test.flac").unwrap())).unwrap(),
+        );
+        sink.play();
+        sink.detach();
+
+        Jukebox {
+            sink: Sink::try_new(&handle).unwrap(),
+            global_settings: GlobalSettings::default(),
+            playback_settings: PlaybackSettings::default(),
+        }
+    }
+
+    fn title(&self) -> String {
+        String::from("Jukebox")
+    }
+
+    fn update(&mut self, event: Message) {
+        match event {
+            Message::PlaybackState(state) => match state {
+                PlaybackState::Play => {
+                    add_song_to_queue(&self.sink, "test.flac")
+                        .expect("adding song to queue failed");
+                    println!("start playback");
+                    println!("{:?}", self.sink.len());
+                    self.sink.play();
+                }
+                PlaybackState::Pause => {
+                    self.sink.pause();
+                }
+                PlaybackState::Stop => {
+                    self.sink.stop();
+                }
+            },
+            Message::AddSongToQueue(song) => {
+                add_song_to_queue(&self.sink, &song).expect("adding song to queue failed")
+            }
+        }
+    }
+
+    fn view(&self) -> Element<Message> {
+        let controls =
+            row![].push(button("Play").on_press(Message::PlaybackState(PlaybackState::Play)));
+        // .push(horizontal_space())
+        // // .push(button("Stop").on_press(Message::StopPlayback))
+        // .push(horizontal_space())
+        // // .push(button("Add song").on_press(Message::AddSongToQueue("test.flac".to_string())));
+
+        container(controls).height(Length::Shrink).center_y().into()
+    }
+}
+
 fn read_tags(path: &str) -> Result<String, JbError> {
     println!("file: {}", path);
     // This will guess the format from the extension
     let tagged_file = read_from_path(path)?;
     Ok(format!("{:?}", tagged_file.file_type()))
 }
-
-#[tauri::command]
-fn get_global_settings() -> Result<GlobalSettings, JbError> {
-    Ok(GlobalSettings::default())
-}
-
-#[tauri::command]
-fn toggle_playback(state: tauri::State<Jukebox>) -> Result<PlaybackState, JbError> {
-    let jb = &state.0;
-    add_song_to_queue(&jb)?;
-    if jb.is_paused() {
-        jb.play();
-        Ok(PlaybackState::Play)
-    } else {
-        jb.pause();
-        Ok(PlaybackState::Pause)
-    }
-}
-
-#[tauri::command]
-fn stop_playback(state: tauri::State<Jukebox>) -> Result<PlaybackState, JbError> {
-    let jb = &state.0;
-    jb.stop();
-    Ok(PlaybackState::Stop)
-}
-
-// Functions
 
 fn read_or_create_config(config_path: &str) -> Result<GlobalSettings, JbError> {
     // TODO check if config file exists, if not create it with defaults, if so read/parse it
@@ -113,28 +165,24 @@ fn read_or_create_config(config_path: &str) -> Result<GlobalSettings, JbError> {
     Ok(settings)
 }
 
-fn add_song_to_queue(jb: &Sink) -> Result<(), JbError> {
-    jb.append(rodio::Decoder::new(BufReader::new(std::fs::File::open(
-        "../test.flac",
+fn add_song_to_queue(sink: &Sink, song: &str) -> Result<(), JbError> {
+    sink.append(rodio::Decoder::new(BufReader::new(std::fs::File::open(
+        song,
     )?))?);
+    println!("added song to queue: {}", song);
     Ok(())
 }
 
-fn main() {
-    // Rodio sink, plays the audio
-    let (_stream, handle) = rodio::OutputStream::try_default().unwrap();
-    let sink = Sink::try_new(&handle).unwrap();
+pub fn main() -> iced::Result {
+    // let (_stream, handle) = rodio::OutputStream::try_default().unwrap();
+    // let sink = Sink::try_new(&handle).unwrap();
+
+    // sink.append(
+    //     rodio::Decoder::new(BufReader::new(std::fs::File::open("test.flac").unwrap())).unwrap(),
+    // );
+    // sink.play();
 
     let settings = read_or_create_config("Settings.toml");
 
-    tauri::Builder::default()
-        .manage(Jukebox(sink))
-        .manage(settings)
-        .invoke_handler(tauri::generate_handler![
-            read_tags,
-            toggle_playback,
-            get_global_settings
-        ])
-        .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+    Jukebox::run(Settings::default())
 }
