3ecfd33fbd84b1ee597d49e9b6486c60b0644e78
jcleme <clemensjosh@proton.me>
2024-05-21 17:20:41 -0400

    swapped from thiserror to anyhow
    blessed.rs is a fantastic resource
    
    this still does not work, but swapping computers so pushed to the hub it
    goes

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,23 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use lofty::read_from_path;
-use lofty::{error::LoftyError, file::TaggedFileExt};
-
+use anyhow::{Context, Ok, Result};
 use figment::{
     providers::{Format, Toml},
     Figment,
 };
-use rodio::Sink;
+use lofty::file::TaggedFileExt;
+use lofty::read_from_path;
+use rodio::{OutputStreamHandle, Sink, Source};
 use serde::{Deserialize, Serialize};
 use state::InitCell;
-use std::fmt::Debug;
+use std::fs::File;
+use std::ops::DerefMut;
 use std::sync::RwLock;
+use std::{collections::VecDeque, fmt::Debug};
 use std::{io::BufReader, path::Path};
-use thiserror::Error;
 
-use cushy::value::{Destination, Dynamic, IntoReader, Source};
+use cushy::value::{Destination, Dynamic, IntoReader, Source as CSource};
 use cushy::widget::MakeWidget;
 use cushy::widgets::button::ButtonKind;
 use cushy::Run;
@@ -41,43 +42,22 @@ impl Default for Settings {
     }
 }
 
-#[derive(Error, Debug)]
-pub enum JbError {
-    #[error("IO Error")]
-    IoError(#[from] std::io::Error),
-    #[error("global settings error")]
-    SettingsError(String),
-    #[error("audio file read error")]
-    AudioFileError(#[from] rodio::decoder::DecoderError),
-    #[error("figment error")]
-    SettingsParsingError(#[from] figment::Error),
-    #[error("lofty error")]
-    AudioTagError(#[from] LoftyError),
-}
-
-// // we must manually implement serde::Serialize
-impl serde::Serialize for JbError {
-    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
-    where
-        S: serde::ser::Serializer,
-    {
-        serializer.serialize_str(self.to_string().as_ref())
-    }
-}
-
 struct Jukebox {
-    sink: Sink,
+    out_handle: OutputStreamHandle,
     state: PlaybackState,
+    now_playing: Option<Sink>,
+    queue: VecDeque<Sink>,
 }
-// struct PlayQueue
 
 impl Default for Jukebox {
     fn default() -> Self {
         let (_stream, handle) = rodio::OutputStream::try_default().unwrap();
-        let sink = Sink::try_new(&handle).unwrap();
+        // let sink = Sink::try_new(&handle).unwrap();
         Self {
-            sink,
+            out_handle: handle,
             state: PlaybackState::Stop,
+            now_playing: None,
+            queue: VecDeque::new(),
         }
     }
 }
@@ -89,42 +69,72 @@ pub enum PlaybackState {
     Stop,
 }
 
+impl PlaybackState {
+    fn change_state(&mut self, state: PlaybackState) -> Self {
+        state
+    }
+}
+
 static JUKEBOX: InitCell<RwLock<Jukebox>> = InitCell::new();
 static GLOBAL_SETTINGS: InitCell<RwLock<Settings>> = InitCell::new();
 
-fn read_tags(path: &str) -> Result<String, JbError> {
+fn read_tags(path: &str) -> Result<String> {
     println!("file: {}", path);
     // This will guess the format from the extension
     let tagged_file = read_from_path(path)?;
     Ok(format!("{:?}", tagged_file.file_type()))
 }
 
-fn get_global_settings() -> Result<Settings, JbError> {
-    Ok(Settings::default())
-}
-
-fn toggle_playback() -> Result<PlaybackState, JbError> {
-    let mut_jb = JUKEBOX.get().write().unwrap();
-    add_song_to_queue(&mut_jb.sink)?;
-    // TODO this it takes 2 clicks to start the first time because the default state is "stopped" not paused
-    if mut_jb.sink.is_paused() {
-        mut_jb.sink.play();
-        println!("playing");
-        Ok(PlaybackState::Play)
-    } else {
-        mut_jb.sink.pause();
-        println!("paused");
-        Ok(PlaybackState::Pause)
+fn toggle_playback() -> Result<()> {
+    let mut mut_jb = JUKEBOX.get().write().unwrap();
+    // let sink = Sink::try_new(&mut_jb.out_handle).unwrap();
+    add_song_to_queue()?;
+    if mut_jb.now_playing.is_none() {
+        mut_jb.now_playing = mut_jb.queue.pop_front();
+        todo!("need to check to make sure now_playing is Some() since queue might return None")
     }
+
+    // TODO this it takes 2 clicks to start the first time because the default state is "stopped" not paused
+    match mut_jb.state {
+        PlaybackState::Play => {
+            mut_jb
+                .now_playing
+                .as_ref()
+                .context("failed to toggle playback")?
+                .pause();
+            mut_jb.state.change_state(PlaybackState::Pause);
+            Ok(())
+        }
+        PlaybackState::Pause => {
+            mut_jb
+                .now_playing
+                .as_ref()
+                .context("failed to toggle playback")?
+                .play();
+            mut_jb.state.change_state(PlaybackState::Play);
+            Ok(())
+        }
+        PlaybackState::Stop => todo!(),
+    }
+    // if mut_jb.state {
+    //     mut_jb.now_playing.unwrap().play();
+    //     // mut_jb.deref_mut().sink.detach();
+    //     println!("playing");
+    //     Ok(())
+    // } else {
+    //     mut_jb.now_playing.unwrap().pause();
+    //     println!("paused");
+    //     Ok(())
+    // }
 }
 
-fn stop_playback() -> Result<PlaybackState, JbError> {
-    let mut_jb = JUKEBOX.get().write().unwrap();
-    mut_jb.sink.stop();
-    Ok(PlaybackState::Stop)
-}
+// fn stop_playback() -> Result<PlaybackState> {
+//     let mut_jb = JUKEBOX.get().write().unwrap();
+//     mut_jb.out_handle.stop();
+//     Ok(PlaybackState::Stop)
+// }
 
-fn read_or_create_config(config_path: &str) -> Result<Settings, JbError> {
+fn read_or_create_config(config_path: &str) -> Result<Settings> {
     // TODO check if config file exists, if not create it with defaults, if so read/parse it
     if !Path::new(config_path).exists() {
         let default_settings = toml::to_string(&Settings::default()).unwrap();
@@ -134,25 +144,23 @@ fn read_or_create_config(config_path: &str) -> Result<Settings, JbError> {
     Ok(settings)
 }
 
-fn add_song_to_queue(jb: &Sink) -> Result<(), JbError> {
-    jb.append(rodio::Decoder::new(BufReader::new(std::fs::File::open(
+fn add_song_to_queue() -> Result<()> {
+    // TODO take song as arg
+    let mut mut_jb = JUKEBOX.get().write().unwrap();
+    let sink = Sink::try_new(&mut_jb.out_handle).unwrap();
+    sink.append(rodio::Decoder::new(BufReader::new(std::fs::File::open(
         "test.flac",
     )?))?);
+    mut_jb.queue.push_back(sink);
     Ok(())
 }
 
 fn main() -> cushy::Result {
-    let (_stream, handle) = rodio::OutputStream::try_default().unwrap();
-    let sink = Sink::try_new(&handle).unwrap();
-
     let settings = read_or_create_config("Settings.toml").unwrap();
 
     let jb = Jukebox::default();
-    // let jb = Dynamic::new(PlaybackState::Play);
     JUKEBOX.set(RwLock::new(jb));
     GLOBAL_SETTINGS.set(RwLock::new(settings));
-
-    // jb.to_label().into_button().on_click(move |sink| )
 
     // Create a dynamic usize.
     let count = Dynamic::new(0_isize);
@@ -167,6 +175,7 @@ fn main() -> cushy::Result {
             toggle_playback().unwrap();
             count.set(count.get() + 1)
         })
+        .centered()
         // Run the application
         .run()
 }
